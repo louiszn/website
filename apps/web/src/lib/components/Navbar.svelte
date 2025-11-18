bb1c601bec6c442e7aa3cb31bed1b9322db37041
Louis Johnson <louiszn269@gmail.com>
2025-11-18 22:42:22 +0700

    feat: page changing transition

--- apps/web/src/lib/components/Navbar.svelte
+++ apps/web/src/lib/components/Navbar.svelte
@@ -21,6 +21,7 @@
 <nav
 	id="navbar"
 	class="flex bg-card/80 outline-card-outline outline-1 backdrop-blur-2xl fixed top-0 w-full py-3 px-20 justify-between items-center z-99"
+	style="view-transition-name: header;"
 >
 	<a href="/" class="font-bold text-xl">Louiszn</a>
 
@@ -34,9 +35,16 @@
 			<a
 				href={p.href}
 				class={`flex hover:text-accent transition-colors items-center gap-2 ${isActive && "text-accent"}`}
+				aria-current={isActive ? "page" : undefined}
 			>
 				{p.label}
 			</a>
 		{/each}
 	</div>
 </nav>
+
+<style>
+	a[aria-current="page"]::before {
+		view-transition-name: active-page;
+	}
+</style>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
+	import { onNavigate } from "$app/navigation";
+	
 	import Navbar from "$lib/components/Navbar.svelte";
-
+	
 	import "../app.css";
 	import favicon from "$lib/assets/favicon.svg";
 	import Background from "$lib/components/Background.svelte";
-	import { afterNavigate } from "$app/navigation";
 
 	let { children } = $props();
 
-	afterNavigate(() => {
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) {
+			return;
+		}
+
 		window.scrollTo({ top: 0 });
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
 	});
 </script>
 
@@ -17,8 +30,7 @@
 	<title>Louiszn</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
-
-<Background  />
+<Background />
 
 <Navbar />
 
