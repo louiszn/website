37563388c6b8c200ce8cd9b261a0d1b43eb4e766
Louis Johnson <louiszn269@gmail.com>
2025-11-18 15:06:23 +0700

    fix: apply smaller design for md

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -87,20 +87,20 @@
 	const weekdays = ["S", "M", "T", "W", "T", "F", "S"];
 </script>
 
-<div class="relative flex flex-col w-full h-screen items-center justify-center gap-10 md:gap-25">
-	<div class="flex items-center gap-6 md:gap-20">
-		<img src={avatar} alt="" class="md:w-60 md:h-60 w-30 h-30 z-10 rounded-full" />
+<div class="relative flex flex-col w-full h-screen items-center justify-center gap-10 lg:gap-25">
+	<div class="flex items-center gap-6 lg:gap-20">
+		<img src={avatar} alt="" class="lg:w-60 lg:h-60 w-30 h-30 z-10 rounded-full" />
 
-		<div class="flex flex-col gap-2 md:gap-5">
-			<div class="flex flex-col gap-1 md:gap-2">
-				<span class="flex flex-col text-2xl md:text-6xl font-bold"
+		<div class="flex flex-col gap-2 lg:gap-5">
+			<div class="flex flex-col gap-1 lg:gap-2">
+				<span class="flex flex-col text-2xl lg:text-6xl font-bold"
 					>Hewwo, I'm <span class="text-accent">Louis Johnson</span></span
 				>
-				<span class="text-md md:text-3xl font-bold text-accent2">A cute oreo kitty</span>
+				<span class="text-lg lg:text-3xl font-bold text-accent2">A cute oreo kitty</span>
 			</div>
 
 			<!-- Links -->
-			<div class="flex items-center gap-3 md:gap-5 h-10">
+			<div class="flex items-center gap-3 lg:gap-5 h-10">
 				{#each links as link (link.url)}
 					<a
 						href={link.url}
@@ -108,40 +108,40 @@
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<Icon icon={link.icon} class="w-6 h-6 md:w-8 md:h-8" />
+						<Icon icon={link.icon} class="w-6 h-6 lg:w-8 lg:h-8" />
 					</a>
 				{/each}
 			</div>
 		</div>
 	</div>
 
-	<div class="md:p-0 p-2 w-full md:w-fit">
+	<div class="lg:p-0 p-2 w-full lg:w-fit">
 		<div
 			class="flex flex-col w-full p-5 gap-3 bg-card/50 backdrop-blur-sm outline-1 outline-card-outline rounded-2xl"
 		>
 			<div class="flex w-full justify-between font-mono">
-				<span class="font-bold md:text-md text-sm">@louiszn's contributions</span>
-				<span class="font-bold md:text-md text-sm"
+				<span class="font-bold lg:text-lg text-sm">@louiszn's contributions</span>
+				<span class="font-bold lg:text-lg text-sm"
 					>{data.contributionCalendar.totalContributions} total</span
 				>
 			</div>
 
 			<div class="flex gap-1">
-				<div class="flex flex-col font-mono text-xs md:text-sm gap-1">
+				<div class="flex flex-col font-mono text-xs lg:text-sm gap-1">
 					{#each weekdays as day, i (i)}
-						<div class="flex w-3 h-3 md:w-4 md:h-4 items-center">{i % 2 !== 0 ? day : ""}</div>
+						<div class="flex w-3 h-3 lg:w-4 lg:h-4 items-center">{i % 2 !== 0 ? day : ""}</div>
 					{/each}
 				</div>
 
 				<div
-					class="grid grid-rows-7 grid-flow-col gap-1 w-full overflow-x-auto md:overflow-hidden"
+					class="grid grid-rows-7 grid-flow-col gap-1 w-full overflow-x-auto lg:overflow-hidden"
 					bind:this={contribGrid}
 				>
 					{#each data.contributionCalendar.weeks as week}
 						{#each week.contributionDays as day}
 							<div
 								title={`${day.date}: ${day.contributionCount} contributions`}
-								class="w-3 md:w-4 h-3 md:h-4 rounded-xs md:rounded-sm cursor-pointer"
+								class="w-3 lg:w-4 h-3 lg:h-4 rounded-xs lg:rounded-sm cursor-pointer"
 								style="background-color: {getColor(day.contributionCount)}"
 							></div>
 						{/each}
@@ -153,7 +153,7 @@
 
 	{#if showArrow}
 		<button
-			class="absolute bottom-25 md:bottom-5 p-2 rounded-full outline outline-muted text-muted animate-bounce"
+			class="absolute bottom-25 lg:bottom-5 p-2 rounded-full outline outline-muted text-muted animate-bounce"
 			in:fade={{ duration: 100 }}
 			out:fade={{ duration: 100 }}
 			onclick={scrollToContent}
