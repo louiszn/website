ec47970db0a91f00c88d6c8b2c98fab1eb3a7531
Louis Johnson <louiszn269@gmail.com>
2025-11-18 22:41:21 +0700

    feat: caching responses

--- apps/web/src/lib/utils/github.ts
+++ apps/web/src/lib/utils/github.ts
@@ -5,21 +5,33 @@ import { GITHUB_TOKEN } from "$env/static/private";
 import GetContribtutions from "$lib/queries/getContributions.gql";
 import type { ContributionCalendar, UserContributions } from "$types/queries/userContributons";
 
+export const DEFAULT_CACHE_TTL = 5 * 60 * 1000;
+
+let cachedCalendar: ContributionCalendar | null = null;
+let lastFetch = 0;
+
 export async function getUserContributionCalendar(username: string): Promise<ContributionCalendar> {
-	const octokit = new Octokit({
-		auth: GITHUB_TOKEN,
-	});
+	const now = Date.now();
 
-	const query = print(GetContribtutions);
+	if (!cachedCalendar || now - lastFetch > DEFAULT_CACHE_TTL) {
+		lastFetch = now;
 
-	const now = new Date();
-	const from = new Date(now.getTime() - 366 * 24 * 60 * 60 * 1_000); // 366 days ago
+		const octokit = new Octokit({
+			auth: GITHUB_TOKEN,
+		});
 
-	const result = await octokit.graphql<UserContributions>(query, {
-		username,
-		from,
-		to: now,
-	});
+		const query = print(GetContribtutions);
 
-	return result.user.contributionsCollection.contributionCalendar;
+		const from = new Date(now - 366 * 24 * 60 * 60 * 1_000); // 366 days ago
+
+		const result = await octokit.graphql<UserContributions>(query, {
+			username,
+			from,
+			to: new Date(now),
+		});
+
+		cachedCalendar = result.user.contributionsCollection.contributionCalendar;
+	}
+
+	return cachedCalendar;
 }
