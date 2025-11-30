ecd7cca27355004f3b0becf249811992006c773f
Louis Johnson <louiszn269@gmail.com>
2025-11-30 15:10:09 +0700

    feat: add page transition

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/state';
+    import { fly } from 'svelte/transition';
+    import { cubicOut } from 'svelte/easing';
+
 	import Navbar from "$lib/components/Navbar.svelte";
 
 	import "../app.css";
@@ -21,6 +25,16 @@
 
 <Navbar />
 
-{@render children()}
+<div class="grid grid-cols-1 grid-rows-1 w-full min-h-screen overflow-x-hidden">
+	{#key page.url.pathname}
+		<div
+			class="row-start-1 col-start-1 w-full"
+			in:fly={{ x: 50, duration: 300, delay: 200, easing: cubicOut }}
+			out:fly={{ x: -50, duration: 200, easing: cubicOut }}
+		>
+			{@render children()}
+		</div>
+	{/key}
+</div>
 
 <Footer />
